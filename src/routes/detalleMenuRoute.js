60c2db8512fa14981fd3a3fed53405639c5ab26f
VS1379 <valentin.silva.user@gmail.com>
2023-11-27 11:28:08 -0300

    In progress 50%

--- src/routes/detalleMenuRoute.js
+++ src/routes/detalleMenuRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const route = express.Router();
 const { detalleMenu } = require("../controllers/detalleMenuController");
 
+route.get("/", detalleMenu);
 route.get("/detalleMenu", detalleMenu);
 
 module.exports = route;
